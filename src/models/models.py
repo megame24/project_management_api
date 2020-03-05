925a938256800fbe936f4b215dbeedbae8fab3b8
Innocent Ngene <30550427+megame24@users.noreply.github.com>
2020-03-05 14:17:50 +0100

    Revert "setup api boiler plate"
    This reverts commit e60db5dbf754565080b960e9ad1932ca944f01b3.

--- src/models/models.py
+++
@@ -1,7 +0,0 @@
-from settings import db
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
--- src/views/auth.py
+++
@@ -1,9 +0,0 @@
-from settings import endpoint
-from flask import jsonify
-from flask_restplus import Resource
-
-
-@endpoint('/login')
-class Login(Resource):
-    def post(self):
-        return jsonify({'name': 'John wick'})
