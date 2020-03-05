319dd3a024bb5751b04407653e1e22a31501f475
Innocent Ngene <ngeneinnocent@gmail.com>
2020-03-05 14:01:15 +0100

    setup api boiler plate

---
+++ app/__init__.py
@@ -0,0 +1,57 @@
+"""This module initializes the API"""
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import app_config
+from app.users.routes import auth_bp
+import dotenv
+
+db = SQLAlchemy()
+dotenv.load_dotenv()
+
+from app.models import seed_db
+
+
+def create_app(env):
+    """
+    Create the flask app instance
+    :param env: current environment
+    :return: Object - Flask instance
+    """
+
+    app = Flask(__name__)
+
+    if env == 'development':
+        import logging
+        logging.basicConfig()
+        logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+    CORS(app, origins=['*'], supports_credentials=True)
+    app.config.from_object(app_config[env])
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    app.register_blueprint(auth_bp, url_prefix='/api')
+
+    @app.before_first_request
+    def initialize():
+        """Seed the db"""
+        seed_db()
+
+    @app.route('/', methods=['GET'])
+    def index():
+        """
+        Index route
+        :return: Json
+        """
+        return jsonify({
+            "message": "Welcome to the proj-m API"
+        })
+
+    return app
+
+
+app = create_app(os.getenv('FLASK_ENV', 'development'))
